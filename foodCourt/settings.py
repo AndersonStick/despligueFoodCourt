6cc7603d88dfe00b352b89a9022124345f091b4f
Anderson Barrera <andersonbar10@gmail.com>
2023-11-12 22:59:30 -0500

    quito media

--- foodCourt/settings.py
+++ foodCourt/settings.py
@@ -140,9 +140,6 @@ STATICFILES_DIRS = [
     'foodCourt/static'
 ]
 
-# Configuración de archivos multimedia
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
